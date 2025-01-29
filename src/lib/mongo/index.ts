97c0ed542bc25fdf3e3b391c878c9b003c83eff0
Hemit Patel <hemitvpatel@gmail.com>
2025-01-29 15:45:43 -0500

    added env

--- src/lib/mongo/index.ts
+++ src/lib/mongo/index.ts
@@ -1,7 +1,7 @@
 import { MongoClient, ServerApiVersion } from "mongodb";
 
 // Create a MongoClient with a MongoClientOptions object to set the Stable API version
-export const client: MongoClient = new MongoClient(process.env.NEXT_PUBLIC_MONGO_URL  || "0" , {
+export const client: MongoClient = new MongoClient(process.env.MONGO_URL  || "0" , {
   serverApi: {
     version: ServerApiVersion.v1,
     strict: true,
